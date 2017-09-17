abd28e4b9ed8b8221d4a69dcdc8329933100a003
Syneil <7725171+Syneil@users.noreply.github.com>
2017-09-17 18:59:57 +0100

    Revert "Some tidy ups"
    This reverts commit 3e221403cf0d26445fa96a57a3ab806c4dffa8cb.

--- src/main/java/net/syneil/fsm/Classifier.java
+++ src/main/java/net/syneil/fsm/Classifier.java
@@ -6,16 +6,15 @@ import java.util.stream.Stream;
  * A classifier is a specialised {@link FSM finite-state machine} that accepts a sequence of symbols and reports the
  * resultant state.
  *
- * @param <S> the type used for states
  * @param <T> the type used for inputs
  * @param <U> the type used for outputs
  */
-public interface Classifier<S, T, U> {
+public interface Classifier<T, U> {
     /**
-     * Accepts all inputs from the stream, if possible, and returns the final state.
+     * Accepts all inputs from the stream, if possible, and returns an output.
      *
      * @param inputs a stream of symbols to input
-     * @return the final state
+     * @return the output
      * @throws NoTransitionForSymbolException if the machine cannot accept one of the inputs
      */
     U acceptAll(Stream<T> inputs);
